1b856e071712460cfa2397278b1f97d45175e4f4
Tushant Man Singh <tushantman77@hotmail.com>
2024-08-23 13:07:00 +1000

    firebase login/signup completed

---
+++ frontend/rt-fall-detection/src/components/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBe7beT9OBEpHryKokfWaE3lJ2a4fasHG4",
+  authDomain: "rt-fall-detection.firebaseapp.com",
+  projectId: "rt-fall-detection",
+  storageBucket: "rt-fall-detection.appspot.com",
+  messagingSenderId: "762693262797",
+  appId: "1:762693262797:web:29ef643ebff4bc5ee5b884",
+  measurementId: "G-D3ZS6QDXS1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth=getAuth();
+export const db=getFirestore(app);
+export default app;
